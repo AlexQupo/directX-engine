820bacd5653cebd0179c657f48d06e6513e7b91e
AlexQupo <43318603+AlexQupo@users.noreply.github.com>
2022-05-05 00:37:31 +0300

    Added DisplayWin32 Class

--- MySuper3DApp/MySuper3DApp.cpp
+++ MySuper3DApp/MySuper3DApp.cpp
@@ -11,6 +11,9 @@
 #include <directxmath.h>
 #include <chrono>
 
+#include "DisplayWin32.h"
+
+
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d3dcompiler.lib")
@@ -34,55 +37,19 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT umessage, WPARAM wparam, LPARAM lparam)
 	}
 }
 
-
 int main()
 {
-	LPCWSTR applicationName = L"My3DApp";
-	HINSTANCE hInstance = GetModuleHandle(nullptr);
 
 #pragma region Window init
-    WNDCLASSEX wc;
 
-	wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = LoadIcon(nullptr, IDI_WINLOGO);
-	wc.hIconSm = wc.hIcon;
-	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
-	wc.hbrBackground = static_cast<HBRUSH>(GetStockObject(BLACK_BRUSH));
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = applicationName;
-	wc.cbSize = sizeof(WNDCLASSEX);
+	//WNDCLASSEX wc;
+	LPCWSTR applicationName = L"My3DApp";
+	int screenWidth = 800;
+	int screenHeight = 800;
+	HINSTANCE hInstance = GetModuleHandle(nullptr);
+	HWND hWnd;
 
-	// Register the window class.
-	RegisterClassEx(&wc);
-
-	auto screenWidth = 800;
-	auto screenHeight = 800;
-
-	RECT windowRect = { 0,0,static_cast<LONG>(screenWidth), static_cast<LONG>(screenHeight) };
-	AdjustWindowRect(&windowRect, WS_EX_OVERLAPPEDWINDOW, FALSE);
-
-	auto dwStyle = WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX | WS_THICKFRAME;
-
-	auto posX = (GetSystemMetrics(SM_CXSCREEN) - screenWidth) / 2;
-	auto posY = (GetSystemMetrics(SM_CYSCREEN) - screenHeight) / 2;
-
-	HWND hWnd = CreateWindowEx(WS_EX_APPWINDOW, applicationName, applicationName,
-		dwStyle,
-		posX, posY,
-		windowRect.right - windowRect.left,
-		windowRect.bottom - windowRect.top,
-		nullptr, nullptr, hInstance, nullptr);
-
-	ShowWindow(hWnd, SW_SHOW);
-	SetForegroundWindow(hWnd);
-	SetFocus(hWnd);
-	
-	ShowCursor(true);
-
+	DisplayWin32(L"My3DApp", screenHeight, screenWidth, hInstance, WndProc, hWnd);
 
 #pragma endregion Window init
 
